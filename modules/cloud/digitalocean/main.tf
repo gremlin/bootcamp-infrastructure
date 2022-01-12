72940ddb2c312eb182bd14e0ffe3a8359a8bc888
Jason Yee <jyee@hellyeah.com>
2022-01-12 22:14:47 +0200

    Remove https

--- modules/cloud/digitalocean/main.tf
+++ modules/cloud/digitalocean/main.tf
@@ -13,15 +13,15 @@ provider "digitalocean" {
 }
 
 # This has to come before the cluster creation
-resource "digitalocean_certificate" "cert" {
-  name = "cert-${var.group_id}"
-  type = "lets_encrypt"
-  domains = ["group${var.group_id}.gremlinbootcamp.com"]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#resource "digitalocean_certificate" "cert" {
+#  name = "cert-${var.group_id}"
+#  type = "lets_encrypt"
+#  domains = ["group${var.group_id}.gremlinbootcamp.com"]
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
 
 resource "digitalocean_kubernetes_cluster" "k8s_cluster" {
   name  = "group-${var.group_id}"
@@ -50,16 +50,16 @@ resource "digitalocean_loadbalancer" "public" {
     entry_protocol = "tcp"
     target_port = 30080
     target_protocol = "tcp"
-    certificate_name = digitalocean_certificate.cert.name
+#    certificate_name = digitalocean_certificate.cert.name
   }
 
-  forwarding_rule {
-    entry_port = 443 
-    entry_protocol = "https"
-    target_port = 30080
-    target_protocol = "http"
-    certificate_name = digitalocean_certificate.cert.name
-  }
+#  forwarding_rule {
+#    entry_port = 443
+#    entry_protocol = "https"
+#    target_port = 30080
+#    target_protocol = "http"
+#    certificate_name = digitalocean_certificate.cert.name
+#  }
 
   healthcheck {
     port = 30080
