dff5f059196e379364e1b4daefd46552c89d9523
Jason Yee <jyee@hellyeah.com>
2022-05-04 11:13:47 -0700

    maybe dont need prefix/namespace

--- modules/monitoring/prometheus_grafana/main.tf
+++ modules/monitoring/prometheus_grafana/main.tf
@@ -2,7 +2,7 @@
 resource "helm_release" "prometheus_helm" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-community/kube-prometheus-stack"
+  chart      = "kube-prometheus-stack"
 
   values = [
     "${replace(file("${path.module}/${var.app}/values.yaml"), "[[GROUP_ID]]", "${var.group_id}")}"
