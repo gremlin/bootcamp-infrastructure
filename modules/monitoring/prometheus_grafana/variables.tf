fe79fcc20e40e306f366e72c36377924d8dd3112
Jason Yee <jyee@hellyeah.com>
2022-05-04 10:45:10 -0700

    try to fix the replace. adding app variable

--- modules/monitoring/prometheus_grafana/variables.tf
+++ modules/monitoring/prometheus_grafana/variables.tf
@@ -2,3 +2,8 @@ variable "group_id" {
   type = string
   description = "The id or number of the bootcamp group."
 }
+
+variable "app" {
+  type = string
+  description = "The demo app that was deployed. This will be used to select the correct dashboard and monitoring configuration."
+}
