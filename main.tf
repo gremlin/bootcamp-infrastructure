f59fc5b868ed8042ecbca3db5a0db28bdeb71cf1
Sam Whyte <sam.whyte@gremlin.com>
2022-07-27 15:10:48 -0600

    Typo :(

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ data "digitalocean_kubernetes_cluster" "k8s_cluster" {
 # Setup the Helm Provider here so that subsequent modules don't need to reinstantiate it.
 provider "helm" {
   kubernetes {
-    host = data.digitalocean_kubernetes_cluster.k8s_cluster.endpointt
+    host = data.digitalocean_kubernetes_cluster.k8s_cluster.endpoint
     token = data.digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].token
     cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.k8s_cluster.kube_config[0].cluster_ca_certificate)
   }
