ee85533117fdd1308ca77a85b66c79f92648b833
Sam Whyte <sam.whyte@gremlin.com>
2022-08-26 09:53:58 -0600

    Adjust outputs

--- examples/kubernetes/digitalocean/outputs.tf
+++ examples/kubernetes/digitalocean/outputs.tf
@@ -1,7 +1,11 @@
 output "environment_count" {
-    value = var.environment.count
+    value = length(module.k8s_envs)
 }
 
 output "k8s_credentials" {
-
+    value = [ for env in module.k8s_envs:
+        endpoint = env.cluster_endpoint
+        token = env.cluster_token
+        cluster_certificate = env.cluster_certificate
+    ]
 }
